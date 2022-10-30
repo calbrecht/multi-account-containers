e430a888aebf5faf381f480583d65a6209acca5c
Christian Albrecht <christian.albrecht@mayflower.de>
2022-10-30 18:58:49 +0100

    Patch assignManager to allow for path in siteKeys

--- src/js/options.js
+++ src/js/options.js
@@ -68,6 +68,7 @@ async function setupOptions() {
   document.querySelector("#replaceTabCheck").checked = !!replaceTabEnabled;
   document.querySelector("#changeTheme").selectedIndex = currentThemeId;
   setupContainerShortcutSelects();
+  setupSitekeyPatterns();
 }
 
 async function setupContainerShortcutSelects () {
@@ -99,6 +100,70 @@ async function setupContainerShortcutSelects () {
   }
 }
 
+async function setupSitekeyPatterns() {
+  //await browser.storage.local.set({
+  //  sitekeyPatterns: [{
+  //    hostname: [
+  //      "(?<mf_google_com>www\\.google\\.com)",
+  //    ],
+  //    pathname: [
+  //      "/calendar/hosted/.*",
+  //      "/a/.*/ServiceLogin",
+  //      "/a/.*/acs",
+  //    ]
+  //  }, {
+  //    hostname: [
+  //      "(?<mf_google_com>www\\.google\\.com)",
+  //    ],
+  //    pathname: [
+  //      "/accounts/signin/continue",
+  //    ],
+  //    search: [
+  //      "continue=.*?calendar\\.google\\.com.*?calendar",
+  //    ]
+  //  }],
+  //});
+
+  const fallback = [{hostname:[], pathname: [], search: []}];
+  const {sitekeyPatterns} = await browser.storage.local.get({sitekeyPatterns: fallback});
+  const container = document.querySelector(`.${SITEKEY_CONTAINER}`);
+
+  container.querySelectorAll(`.${SITEKEY_PATTERN}`).forEach(p => p.remove());
+
+  sitekeyPatterns.forEach(pattern => {
+    const patternNode = createSitekeyPattern();
+    container.append(patternNode);
+    Object.entries(pattern).forEach(([partId, rows]) => {
+      const partNode = patternNode.querySelector(`[data-part-id=${partId}]`);
+      rows.forEach((regexp, index) => {
+        let rowNode;
+        if (index > 0) {
+          rowNode = createSitekeyRow(partId);
+          partNode.append(rowNode);
+        } else {
+          rowNode = partNode.querySelector(`.${SITEKEY_ROW}`);
+        }
+        rowNode.querySelector("input").value = regexp;
+      });
+    });
+  });
+
+  updateSitekeyDisabledStates();
+}
+
+function storeSitekeyPatterns() {
+  const filter = obj => Object.fromEntries(Object.entries(obj).filter(([,a]) => a.length));
+  const values = inputs => [...inputs].map(i => i.value).filter(s => !!s);
+  const sitekeyPatterns = [...document.querySelectorAll(`.${SITEKEY_PATTERN}`)].map(
+    pattern => [...pattern.querySelectorAll("[data-part-id]")].slice(1).reduce(
+      (result, part) => filter({
+        ...result,
+        [part.dataset.partId]: values(part.querySelectorAll("input"))
+      }), {})
+  );
+  browser.storage.local.set({sitekeyPatterns});
+}
+
 function storeShortcutChoice (event) {
   browser.runtime.sendMessage({
     method: "setShortcut",
@@ -109,6 +174,185 @@ function storeShortcutChoice (event) {
 
 function resetOnboarding() {
   browser.storage.local.set({"onboarding-stage": 0});
+}
+
+const SITEKEY_CONTAINER = "sitekey-pattern-container";
+const SITEKEY_PATTERN = "sitekey-pattern";
+const SITEKEY_PART = "sitekey-part";
+const SITEKEY_ROW = "sitekey-row";
+const SITEKEY_ADD = "sitekey-add";
+const SITEKEY_REMOVE = "sitekey-remove";
+
+function enabledSitekeyFormElementsIn(node) {
+  return node.querySelectorAll("input:not([disabled]),button:not([disabled])");
+}
+
+function updateSitekeyDisabledStates() {
+  const someInputsWith = predicate => node =>
+    [...node.querySelectorAll("input")].some(predicate);
+  const someFilledInputs = someInputsWith(input => input.value !== "");
+  const someEmptyInputs = someInputsWith(input => input.value === "");
+  const relatedInput = button => button.closest(`.${SITEKEY_ROW}`).querySelector("input");
+
+  const patternSelector = `.${SITEKEY_PART}[data-part-id=pattern]`;
+  const addButtonSelector = `[data-btn-id="${SITEKEY_ADD}"]`;
+  const removeButtonSelector = `[data-btn-id="${SITEKEY_REMOVE}"]`;
+
+  document.querySelectorAll(`${patternSelector} ${removeButtonSelector}`).forEach(
+    (button, index, list) => button.disabled = list.length === 1
+  );
+  document.querySelectorAll(`${patternSelector} ${addButtonSelector}`).forEach(
+    button => button.disabled = !someFilledInputs(button.closest(`.${SITEKEY_PATTERN}`))
+  );
+  document.querySelectorAll(`.${SITEKEY_PATTERN}`).forEach(
+    p => p.querySelectorAll(`.${SITEKEY_PART}:not([data-part-id=pattern])`).forEach(
+      (part, pIndex, pList) => {
+        const isFirstPart = pIndex === 0;
+        const prevPartHasFilledInputs = pIndex > 0 && someFilledInputs(pList[pIndex - 1]);
+
+        part.querySelectorAll("input").forEach(
+          (input, index, list) => {
+            const theFirstOne  = index === 0 && isFirstPart;
+            const prevPartHasValue = index === 0 && prevPartHasFilledInputs;
+            const prevInputHasValue = index > 0 && list[index - 1].value !== "";
+            input.disabled = !(theFirstOne || prevPartHasValue || prevInputHasValue);
+          }
+        );
+
+        part.querySelectorAll(removeButtonSelector).forEach(
+          (button, index, list) => {
+            const theOnlyOne = list.length === 1;
+            const hasEmptySiblings = someEmptyInputs(part);
+            const relatedIsFilled = relatedInput(button).value !== "";
+            button.disabled = theOnlyOne || (hasEmptySiblings && relatedIsFilled);
+          }
+        );
+
+        part.querySelectorAll(addButtonSelector).forEach(
+          button => {
+            const hasEmptySiblings = someEmptyInputs(part);
+            const relatedIsEmpty = relatedInput(button).value === "";
+            button.disabled = relatedIsEmpty || hasEmptySiblings;
+          }
+        );
+      }
+    )
+  );
+}
+
+function updateSitekeyPatternsOnChange() {
+  updateSitekeyDisabledStates();
+  storeSitekeyPatterns();
+}
+
+function registerSitekeyInput(input) {
+  input.addEventListener("keydown", (event) => {
+    if (["Enter", "Tab"].includes(event.code)) {
+      event.code === "Enter" && event.preventDefault();
+      updateSitekeyPatternsOnChange();
+    }
+  });
+  input.addEventListener("change", updateSitekeyPatternsOnChange);
+}
+
+function createSitekeyInput() {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = "";
+  registerSitekeyInput(input);
+  return input;
+}
+
+function createSitekeyButton(btnId) {
+  const button = document.createElement("button");
+  button.dataset.btnId = btnId;
+  button.disabled = true;
+  button.type = "button";
+  button.textContent = btnId === SITEKEY_REMOVE ? "-" : "+";
+  button.addEventListener("click", onClickSitekeyButton);
+  return button;
+}
+
+function createSitekeyRow(urlpart) {
+  const el = document.createElement("div");
+  el.className = SITEKEY_ROW;
+
+  if (urlpart !== "pattern") {
+    el.append(createSitekeyInput());
+  }
+
+  el.append(createSitekeyButton(SITEKEY_REMOVE));
+  el.append(createSitekeyButton(SITEKEY_ADD));
+
+  return el;
+}
+
+function createSitekeyPart(urlpart) {
+  const part = document.createElement("div");
+  const span = document.createElement("span");
+
+  if (urlpart === "pattern") {
+    span.textContent = "Sitekey Pattern";
+  } else {
+    span.textContent = `${urlpart}:`;
+  }
+
+  part.dataset.partId = urlpart;
+  part.className = SITEKEY_PART;
+  part.append(span);
+  part.append(createSitekeyRow(urlpart));
+
+  return part;
+}
+
+function createSitekeyPattern() {
+  const el = document.createElement("div");
+  el.className = SITEKEY_PATTERN;
+  el.append(createSitekeyPart("pattern"));
+  el.append(createSitekeyPart("hostname"));
+  el.append(createSitekeyPart("pathname"));
+  el.append(createSitekeyPart("search"));
+
+  return el;
+}
+
+function addSitekeyNodeAfter(node) {
+  let insertNode;
+
+  if (node.classList.contains(SITEKEY_ROW)) {
+    insertNode = createSitekeyRow(node.closest(`.${SITEKEY_PART}`).dataset.partId);
+  } else {
+    insertNode = createSitekeyPattern();
+  }
+
+  node.after(insertNode);
+  updateSitekeyDisabledStates();
+  insertNode.querySelector("input").focus();
+}
+
+function removeSitekeyNode(node) {
+  const {nextElementSibling, previousElementSibling} = node;
+
+  node.remove();
+  updateSitekeyDisabledStates();
+
+  if (nextElementSibling) {
+    enabledSitekeyFormElementsIn(nextElementSibling).item(0).focus();
+  } else {
+    [...enabledSitekeyFormElementsIn(previousElementSibling)].at(-1).focus();
+  }
+}
+
+function onClickSitekeyButton(event) {
+  const button = event.target;
+  const isPattern = button.closest(`.${SITEKEY_PART}`).dataset.partId === "pattern";
+  const node = button.closest(isPattern ? `.${SITEKEY_PATTERN}` : `.${SITEKEY_ROW}`);
+
+  if (button.dataset.btnId === SITEKEY_REMOVE) {
+    removeSitekeyNode(node);
+  } else {
+    addSitekeyNodeAfter(node);
+  }
 }
 
 async function resetPermissionsUi() {
@@ -131,8 +375,10 @@ for (let i=0; i < NUMBER_OF_KEYBOARD_SHORTCUTS; i++) {
     .addEventListener("change", storeShortcutChoice);
 }
 
+document.querySelectorAll(`.${SITEKEY_PATTERN} input`).forEach(registerSitekeyInput);
+
 document.querySelectorAll("[data-btn-id]").forEach(btn => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", (event) => {
     switch (btn.dataset.btnId) {
     case "reset-onboarding":
       resetOnboarding();
@@ -141,6 +387,10 @@ document.querySelectorAll("[data-btn-id]").forEach(btn => {
       browser.tabs.create({
         url: MozillaVPN.attachUtmParameters("https://support.mozilla.org/kb/protect-your-container-tabs-mozilla-vpn", "options-learn-more")
       });
+      break;
+    case SITEKEY_REMOVE:
+    case SITEKEY_ADD:
+      onClickSitekeyButton(event);
       break;
     }
   });
